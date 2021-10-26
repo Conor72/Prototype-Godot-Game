aea603170d2a1ca7f6cb287d894e1d9b929578d2
Conor Brett <conorbrett72@gmail.com>
2021-10-26 01:05:21 +0100

    Basic movement + jumping added
    Left and right movement + jumping mechanics added

--- Platformer/Scripts/Player.gd
+++ Platformer/Scripts/Player.gd
@@ -1,13 +1,34 @@
-extends Node2D
+extends "res://Scripts/Actor.gd"
 onready var animated_sprite = $AnimatedSprite
 
 var velocity = Vector2.ZERO
 var max_run = 100
 var run_accel = 800
+var gravity = 1000
+var max_fall = 160
+var jump_force = -160 
+var jump_hold_time = 0.2
+var local_hold_time = 0
 
 func _process(delta):
-	
+	#
 	var direction = sign(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
+	var on_ground = global_position.y >= 160
+	
+	var jumping = Input.is_action_pressed("jump")
+	if jumping && on_ground:
+		velocity.y = jump_force
+		local_hold_time = jump_hold_time
+	elif local_hold_time > 0:
+		if jumping:
+			velocity.y = jump_force
+		else:
+			local_hold_time = 0
+			
+			
+	local_hold_time -= delta
+				
+	
 	
 	if direction > 0:					#If player is facing left or right change direction of sprite too
 		animated_sprite.flip_h = false
@@ -22,5 +43,9 @@ func _process(delta):
 	
 	
 	velocity.x = move_toward(velocity.x, max_run * direction, run_accel * delta)
-	global_position.x += (velocity.x * delta)
+	velocity.y = move_toward(velocity.y, max_fall , gravity * delta)
+	
+	move_x(velocity.x * delta) #Movement from Actor script
+	move_y(velocity.y * delta)
+		
 	
